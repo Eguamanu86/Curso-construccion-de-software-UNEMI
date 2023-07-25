196d120648f98a1f4dd214f7a311043c7aeddb51
Ernesto Guaman <ernesto.guaman@enviame.io>
2023-07-24 19:05:31 -0500

    menu principal django

--- django-backend/core/urls.py
+++ django-backend/core/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from seguridad.views import HomeView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('seguridad/', include('seguridad.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
