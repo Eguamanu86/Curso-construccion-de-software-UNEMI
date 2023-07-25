0c608f7640dcc9ce05b2bde6ee23ec976f915dfe
Ernesto Guaman <ernesto.guaman@enviame.io>
2023-07-24 19:54:09 -0500

    menu principal WIP

--- django-backend/core/urls.py
+++ django-backend/core/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('seguridad/', include('seguridad.urls')),
+    path('inventario/', include('inventario.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django-backend/inventario/views.py
+++ django-backend/inventario/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+class ProductoListView(LoginRequiredMixin, ListView):
+    login_url = '/seguridad/login/'
+    redirect_field_name = 'redirect_to'
+    template_name = 'inventario/productos.html'
+    context_object_name = 'productos'
+    paginate_by = 10
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+    def get_queryset(self, **kwargs):
+        return []
